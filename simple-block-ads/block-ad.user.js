a9070f3136d83c7bd6cac1896a21c5d2dfc24a8d
elevenmox <elevenzen@126.com>
2017-12-24 13:18:36 +0800

    one simple script to block ads

---
+++ simple-block-ads/block-ad.user.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @name           Just for testing Chrome APP
+// @description    Yes
+// @version        1.0
+// @author         eleven
+// @namespace      eleven.net
+// @include        *
+// ==/UserScript==
+window.addEventListener('load', function() {
+  //filter
+  let adFilter = ".adsbygoogle";
+  adFilter += ",iframe[src*='pos.baidu.com']";
+  adFilter += ",[id^=_],[id^=ad_],[id*=-ad-],[id*=_ad_],[class^=ad_],[class*=-ad-],[class*=_ad_]";
+  //find and remove
+  let adNodes = document.querySelectorAll(adFilter);
+  Array.from(adNodes).forEach( n => n.remove());
+}, false);
