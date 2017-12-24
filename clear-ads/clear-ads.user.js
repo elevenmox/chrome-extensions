9c39c17637a673328a0e7fcfb6b6838928deaa28
elevenmox <elevenzen@126.com>
2017-12-24 17:45:19 +0800

    anthor method to clear ads in chrome

---
+++ clear-ads/clear-ads.user.js
@@ -0,0 +1,16 @@
+﻿
+//filters
+let filters = [
+  ".adsbygoogle",
+  "iframe[src*='pos.baidu.com']",
+  "[id^=_],[id^=ad_],[id*=-ad-],[id*=_ad_]",
+  "[class^=ad_],[class*=-ad-],[class*=_ad_]",
+  "#daily_topic_flnr, #todayFix",
+  ".IL_BASE" //infolinks
+];
+let selector = filters.join(",");
+console.log(selector);
+
+//find and remove
+let adNodes = document.querySelectorAll(filters.join(","));
+Array.from(adNodes).forEach( n => n.remove());
